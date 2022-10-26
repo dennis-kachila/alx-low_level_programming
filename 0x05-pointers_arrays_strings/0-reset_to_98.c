53c455328dc1ed11e72c899f1500e9e5093b55ed
Dennis Kachila <denniskachila4332@gmail.com>
2022-10-26 13:12:28 +0300

    betty doc

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * @reset_to_98 changes value of n
- * function that takes a pointer to an int type
- * and updates it
- * return 0
+  * reset_to_98 - this function does not modify n
+ * @n: a useless integer
+ *
+ * Return: nothing.
  */
 
 void reset_to_98(int *n)
