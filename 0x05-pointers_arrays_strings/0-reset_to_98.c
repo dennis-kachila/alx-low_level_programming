88d9b4b4f60af3f40b4107c290843105cf17bbd7
Dennis Kachila <denniskachila4332@gmail.com>
2022-10-26 13:07:18 +0300

    first task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * @reset_to_98 changes value of n
+ * function that takes a pointer to an int type
+ * and updates it
+ * return 0
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
+
